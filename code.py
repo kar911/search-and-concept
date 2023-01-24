39e52e19c310c67c1e6bc3fd4dea1ef969b55448
Kartik <51226698+kar911@users.noreply.github.com>
2023-01-24 13:06:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -41,45 +41,44 @@ def kartik_strafe_s(arr,x,rp2,rp,l):
         return -1
 
 
-# print(arr[:10],l)
 
-#print("kartik_strafe_s 3 13","\n")
-#print("---------------------------AVG tim")
-#xx=[]
-#for i in arr:
-#    start= timer()
-#    kartik_strafe_s(arr,i,rp2,f11,l)
-#    end= timer()
-#    xx.append(end-start)
+print("kartik_strafe_s 3 13","\n")
+print("---------------------------AVG tim")
+xx=[]
+for i in arr:
+    start= timer()
+    kartik_strafe_s(arr,i,rp2,f11,l)
+    end= timer()
+    xx.append(end-start)
     #data= Counter([ round(i,6) for  i in xx])
     #print("--",data.most_common(10))
-#print(sum(xx)/(l+1),max(xx),min(xx))
-#
-#print("---------------------------not there AVG tim")
-#xx=[]
-#for i in arr:
-#    start= timer()
-#    kartik_strafe_s(arr,i+0.1,rp2,f11,l)
-#    end= timer()
-#    xx.append(end-start)
+print(sum(xx)/(l+1),max(xx),min(xx))
+
+print("---------------------------'not there' AVG tim")
+xx=[]
+for i in arr:
+    start= timer()
+    kartik_strafe_s(arr,i+0.1,rp2,f11,l)
+    end= timer()
+    xx.append(end-start)
     #data= Counter([ round(i,6) for  i in xx])
     #print("--",data.most_common(10))
-#print(sum(xx)/(l+1),max(xx),min(xx)) 
-#
-#
-#print("---------------------------agg tim")
-#start= timer()
-#cc=[ kartik_strafe_s(arr,i,rp2,f11,l) for i in arr]
-#end= timer()
-#print(end-start)
-#
-#print("---------------------------not there agg tim")
-#start= timer()
-#cc=[ kartik_strafe_s(arr,i+0.1,rp2,f11,l) for i in arr]
-#end= timer()
-#print(end-start)
-#
-#print("---------------------------non uniform all good")
-#cc=[ kartik_strafe_s(arr,i,rp2,f11,l) for i in arr] #----------------------
-#cc2=[ i  for i in range(l+1) if cc[i]  == -1 or cc[i] != i ] #----------------------
-#print(cc2[:10],len(cc2))
+print(sum(xx)/(l+1),max(xx),min(xx)) 
+
+
+print("---------------------------total tim")
+start= timer()
+cc=[ kartik_strafe_s(arr,i,rp2,f11,l) for i in arr]
+end= timer()
+print(end-start)
+
+print("---------------------------'not there' total tim")
+start= timer()
+cc=[ kartik_strafe_s(arr,i+0.1,rp2,f11,l) for i in arr]
+end= timer()
+print(end-start)
+
+print("---------------------------non uniform all good")
+cc=[ kartik_strafe_s(arr,i,rp2,f11,l) for i in arr] #----------------------
+cc2=[ i  for i in range(l+1) if cc[i]  == -1 or cc[i] != i ] #----------------------
+print(cc2[:10],len(cc2))
