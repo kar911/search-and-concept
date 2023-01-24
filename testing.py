6312d9a8acdd796046a240600057e3aea8591729
Kartik <51226698+kar911@users.noreply.github.com>
2023-01-24 12:57:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,9 +3,9 @@ from timeit import default_timer as  timer
 arr=mm.random_combination(range(1,10005000),r=10000000)
 l=len(arr)
 del mm
-f1=3
-f11=13
-f21=55
+f1=1
+f11=3
+f21=13
 while (f21 < l):
     f1,f11,f21=f11,f21,(f21<<2)+f11  #----------------------
 l=l-1
@@ -223,49 +223,49 @@ print(f1,f11,f21)
     #rn2=(f11<<1 )+gg
     #rp2=(rn2<<1 )-f11
 
-rp2=f21
-f11=f11
+# rp2=f21
+# f11=f11
 
-print(l ,"hello 2 21 233 ")
+# print(l ,"hello 2 21 233 ")
 
-def kartik_strafe_ss(arr,x,rp2,rp,l): # arr,x,rp1=0,rn1=0,fa1=0,fa2=0,x1=None
-#    global c
-#    c=0
-    rn1=0
-    c2=rn1+rp
-    x2=arr[c2]
-    #def kartik_strafe_s(arr,x,rp2,rp,l,n2,c2,x2): # arr,x,rp1=0,rn1=0,fa1=0,fa2=0,x1=None
-    if x == arr[l]:
-        return l
-    gg=True
-    while(rp > 0 ):
-#        c+=1
-#        print(rp,rp2)
-        if x < x2:
-            l=c2
-            rp,rp2=rp2-(rp*11),rp
-            gg=False
-        elif x > x2:
-            rn1=c2 #1.1.2
-            if gg:
-                while ( rp > l-rn1): # edited last
-                    rp,rp2=rp2-(rp*11),rp
-            gg=True
+# def kartik_strafe_ss(arr,x,rp2,rp,l): # arr,x,rp1=0,rn1=0,fa1=0,fa2=0,x1=None
+# #    global c
+# #    c=0
+#     rn1=0
+#     c2=rn1+rp
+#     x2=arr[c2]
+#     #def kartik_strafe_s(arr,x,rp2,rp,l,n2,c2,x2): # arr,x,rp1=0,rn1=0,fa1=0,fa2=0,x1=None
+#     if x == arr[l]:
+#         return l
+#     gg=True
+#     while(rp > 0 ):
+# #        c+=1
+# #        print(rp,rp2)
+#         if x < x2:
+#             l=c2
+#             rp,rp2=rp2-(rp*11),rp
+#             gg=False
+#         elif x > x2:
+#             rn1=c2 #1.1.2
+#             if gg:
+#                 while ( rp > l-rn1): # edited last
+#                     rp,rp2=rp2-(rp*11),rp
+#             gg=True
 
-        else :
-            return c2            
-        c2=(rn1+rp)<<2
-        x2=arr[c2]
-    else:
+#         else :
+#             return c2            
+#         c2=(rn1+rp)<<2
+#         x2=arr[c2]
+#     else:
         
-        if l > rn1+rp+1 and x == arr[rn1+rp+1]:
-            return rn1+rp+1
-        elif l > rn1+rp+2 and x == arr[rn1+rp+2]:
-            return rn1+rp+2
-        return -1
+#         if l > rn1+rp+1 and x == arr[rn1+rp+1]:
+#             return rn1+rp+1
+#         elif l > rn1+rp+2 and x == arr[rn1+rp+2]:
+#             return rn1+rp+2
+#         return -1
 
 
-print("kartik_strafe_sss","\n")
+# print("kartik_strafe_sss","\n")
 #print("---------------------------AVG tim")
 #xx=[]
 #for i in arr:
@@ -308,10 +308,10 @@ print("kartik_strafe_sss","\n")
 #print(end-start)
 #
 
-print("---------------------------non uniform all good")
-cc=[ kartik_strafe_ss(arr,i,rp2,f11,l) for i in arr] #----------------------
-cc2=[ i  for i in range(l+1) if cc[i]  == -1 or cc[i] != i ] #----------------------
-print(cc2[:10],len(cc2))
+# print("---------------------------non uniform all good")
+# cc=[ kartik_strafe_ss(arr,i,rp2,f11,l) for i in arr] #----------------------
+# cc2=[ i  for i in range(l+1) if cc[i]  == -1 or cc[i] != i ] #----------------------
+# print(cc2[:10],len(cc2))
 
 
 
